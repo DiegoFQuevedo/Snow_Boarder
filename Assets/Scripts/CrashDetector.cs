9b1965df50b08c88aec8aeb217f044d5f61067c8
DiegoFQuevedo <quevedodiego7@gmail.com>
2022-10-05 13:04:18 -0300

    Particles effects
    Added a system of particles effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,12 +6,14 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float reloadTime = 1f;
+    [SerializeField] ParticleSystem crashEffect;
     
   
     void OnTriggerExit2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene",reloadTime);
         }
     }
